5eb85b524763037e8739f69ce69112392f90bd41
若水 <168269104+Crystal0716@users.noreply.github.com>
2024-06-13 20:20:53 +0800

    Add files via upload

---
+++ class03.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std ;
+int main ()
+{
+	char x[32] ;
+	cout << x << " Enter your ID: " ;
+	cin >> x ;
+	cout << " Your ID is " << x << ",thank you ." << endl ;
+
+	return (0);
+}
